ded9fcb821572e529140a1ae92e79adf09e319e5
Mohamed Magdy <105607706+MohamedMagdy66@users.noreply.github.com>
2025-11-14 00:04:22 +0200

    Add  reviews controller logic

--- backend/Controllers/ReviewsController.cs
+++ backend/Controllers/ReviewsController.cs
@@ -1,12 +1,68 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers
 {
-    public class ReviewsController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class ReviewsController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IReviewRepository _reviewRepository;
+        public ReviewsController(IReviewRepository reviewRepository)
         {
-            return View();
+            _reviewRepository = reviewRepository;
         }
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> CreateReviewAsync([FromBody] CreateReviewDto createReviewDto)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if(userId == null)
+            {
+                return Unauthorized("User ID not found in token.");
+            }
+            try 
+            {
+                var newreviewDto = await _reviewRepository.CreateReviewAsync(createReviewDto, userId);
+                return Ok(newreviewDto);
+
+            }
+            catch(KeyNotFoundException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch(InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch(Exception)
+            {
+                return StatusCode(500, "An unexpected error occurred while creating the review.");
+            }
+        }
+        [HttpGet("room/{roomId}")]
+        public async Task<IActionResult> GetReviewsByRoomIdAsync(int roomId)
+        {
+            var reviews = await _reviewRepository.GetReviewsByRoomIdAsync(roomId);
+            return Ok(reviews);
+        }
+        // --- 3. Endpoint: حذف تقييم (للأدمن فقط) ---
+        // DELETE: /api/reviews/10
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Admin")] // (محمي للأدمن فقط)
+        public async Task<IActionResult> DeleteReview(int id)
+        {
+            var result = await _reviewRepository.DeleteReviewAsync(id);
+
+            if (!result)
+            {
+                // لو المطبخ قال "false" (ملقاش الريفيو)
+                return NotFound($"Review with ID {id} not found.");
+            }
+
+            // لو المطبخ قال "true" (تم الحذف)
+            return NoContent();
+        }
+
     }
 }
